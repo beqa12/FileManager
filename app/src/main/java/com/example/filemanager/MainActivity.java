4244f54cebd39fdd5c23536f58c11ebc1700f903
beqa <bekach@leavingstone.com>
2021-01-21 21:08:10 +0400

    sout added

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -49,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements FileRecyclerAdaap
         adaapter.addFiles(Arrays.asList(getFiles()));
         adaapter.setFileItemClickListener(this);
         setClickClicklisteners();
+
+        System.out.println("I am working");
     }
 
     private void setClickClicklisteners() {
